4007d2acb7fd3a2e75e2d75413af891908729864
omololevy <cotechlevy@gmail.com>
2022-05-23 02:33:46 +0300

    Create the data log file

---
+++ data_log.py
@@ -0,0 +1,8 @@
+from http import server
+import paho.mqtt. client as mqtt
+
+for received data from server
+def on_connect(data_iot, user,events):
+    print("connected with code " + str(events))
+
+data = mqtt.Client()
