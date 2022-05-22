f49fb1c45a1bfefbb9de041dfc9547e1badcd408
omololevy <cotechlevy@gmail.com>
2022-05-23 02:40:03 +0300

    Add the on connect variiable

--- data_log.py
+++ data_log.py
@@ -6,3 +6,6 @@ def on_connect(data_iot, user,events):
     print("connected with code " + str(events))
 
 data = mqtt.Client()
+Data.on_connect = on_connect
+Data.on_message =on_message
+data.loop_forever()
